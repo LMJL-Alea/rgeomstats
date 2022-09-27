574d994b999a4074df7c18c85ae12f5b0dabec41
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-09-27 21:17:49 +0200

    Skip tests in no python.

--- tests/testthat/test-spd-matrices.R
+++ tests/testthat/test-spd-matrices.R
@@ -1,9 +1,11 @@
 test_that("SPDMatrices constructor works", {
+  skip_if_no_python()
   spdm <- SPDMatrices$new(n = 3)
   expect_true("SPDMatrices" %in% class(spdm))
 })
 
 test_that("SPDMatrices method belongs() works", {
+  skip_if_no_python()
   spdm <- SPDMatrices$new(n = 3)
   A <- diag(1, 3)
   expect_true(spdm$belongs(A))
@@ -12,6 +14,7 @@ test_that("SPDMatrices method belongs() works", {
 })
 
 test_that("SPDMatrices method cholesky_factor() works", {
+  skip_if_no_python()
   spdm <- SPDMatrices$new(n = 3)
   V <- cbind(
     c(sqrt(2) / 2, -sqrt(2) / 2, 0),
